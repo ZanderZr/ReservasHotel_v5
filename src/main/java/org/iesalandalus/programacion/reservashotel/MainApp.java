c7529375121ea6cc84326ef9b915eddcf70175d5
JuanCarlosZr <jc.zr88@gmail.com>
2024-02-16 11:01:39 +0100

    Primeros pasos commit

--- src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/reservashotel/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp {
 
 
     public static void main(String[] args) {
-        System.out.println("Programa para la Gestión de Hoteles IES Al-Ándalus");
+        System.out.println("Programa para la Gestión de Hoteles IES  Al-Ándalus");
 
     }
 
