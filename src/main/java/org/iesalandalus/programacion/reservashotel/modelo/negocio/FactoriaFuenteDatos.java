e27ca280cdf681951aebe48aa2943713362a9813
JuanCarlosZr <jc.zr88@gmail.com>
2024-04-05 13:33:11 +0200

    Fuente de datos para el modelo

---
+++ src/main/java/org/iesalandalus/programacion/reservashotel/modelo/negocio/FactoriaFuenteDatos.java
@@ -0,0 +1,24 @@
+package org.iesalandalus.programacion.reservashotel.modelo.negocio;
+
+import org.iesalandalus.programacion.reservashotel.modelo.negocio.mongodb.FuenteDatosMongoDB;
+import org.iesalandalus.programacion.reservashotel.modelo.negocio.mongodb.memoria.FuenteDatosMemoria;
+
+public enum FactoriaFuenteDatos {
+
+        MEMORIA {
+            @Override
+            public IFuenteDatos crearFuenteDatos() {
+                return new FuenteDatosMemoria();
+            }
+        },
+
+        MONGODB {
+            @Override
+            public IFuenteDatos crearFuenteDatos() {
+                return new FuenteDatosMongoDB();
+            }
+        };
+
+        public abstract IFuenteDatos crearFuenteDatos();
+    }
+
